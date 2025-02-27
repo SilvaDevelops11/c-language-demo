cae3f38897bdab50ddd26691af860f0480db02ca
Alvis Gordon <alvisgorden07@hotmail.com>
2025-02-27 14:27:49 +1100

    hey

--- whatismyname.c
+++ whatismyname.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 
 int main() {
-    char name[20];
-    printf("What is your name? ");
-    scanf("%s", name);
-    printf("Hello, %s!\n", name);
-    printf("Goodbye, %s!\n", name);
+    char firstName[20];
+    char lastName[20];
+    int age;
+
+    printf("What is your first name? ");
+    scanf("%s", firstName);
+    printf("What is your last name? ");
+    scanf("%s", lastName);
+    printf("How old are you? ");
+    scanf("%d", &age);
+
+    printf("You entered: %s %s, Age: %d\n", firstName, lastName, age);
+    printf("Hello, %s %s!\n", firstName, lastName);
+    printf("You are %d years old.\n", age);
+
     return 0;
 }
