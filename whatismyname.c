3123861eadf6983d974e90e040055f21aa39e313
Alvis Gordon <alvisgorden07@hotmail.com>
2025-02-27 14:32:45 +1100

    age?

--- whatismyname.c
+++ whatismyname.c
@@ -3,9 +3,13 @@
 
 int main() {
     char name[20];
+    char age[3];
     printf("What is your name? ");
     scanf("%s", name);
     printf("Hello, %s!\n", name);
-    printf("Goodbye, %s!\n", name);
+    printf("what is your age? so I can tell you how old you will be in 10 years\n");
+    scanf("%s", age);
+    int age_int = atoi(age);
+    printf("In 10 years you will be %d years old\n", age_int + 10);
     return 0;
 }
