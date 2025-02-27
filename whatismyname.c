1f606a742e017156fba5fd231d79dfcde6bd6e30
Alvis Gordon <alvisgorden07@hotmail.com>
2025-02-27 13:30:51 +1100

    Update whatismyname.c

--- whatismyname.c
+++ whatismyname.c
@@ -6,5 +6,14 @@ int main() {
     printf("What is your name? ");
     scanf("%s", name);
     printf("Hello, %s!\n", name);
+    printf("where are you from, %s?\n", name);
+    char place[30];
+    scanf("%s", place);
+    printf("I see, %s is a nice place!\n", name);
+
+
+
+
+
     return 0;
 }
